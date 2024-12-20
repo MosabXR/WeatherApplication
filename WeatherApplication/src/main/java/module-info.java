adcc87cd32e337a6fc00ccf226113e86e148a801
MosabXR <117486800+MosabXR@users.noreply.github.com>
2024-12-20 22:12:31 +0200

    Delete WeatherApplication directory

--- WeatherApplication/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.weatherapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.google.gson; 
-
-    opens com.mycompany.weatherapplication to com.google.gson, javafx.fxml;
-    exports com.mycompany.weatherapplication;
-}
